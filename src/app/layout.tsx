6732e9840c8e4e5fdf91a6621dff24d0eb57c1db
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2024-11-30 23:51:42 +0530

    base files setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const fontHeading = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
+});
+
+const fontBody = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+});
+
+export const metadata: Metadata = {
+  title: "Learning Management System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fontHeading.variable} ${fontBody.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
