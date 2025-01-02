cd3ab928f2eaf2a20412b0888723214fd2ddb765
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-01-02 14:19:33 +0530

    added all courses page

--- src/app/(public)/course/page.tsx
+++ src/app/(public)/course/page.tsx
@@ -1,7 +1,20 @@
+import { getAllCourses } from "@/actions/course";
+import CourseCard from "../_components/CourseCard";
+
 const Page = async () => {
+  const { courses } = await getAllCourses();
+
   return (
-    <div>
-      <h1>Course Home</h1>
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl text-primary font-bold text-center">
+        All Courses
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {courses?.map((course) => (
+          <CourseCard key={course?.course_id} course={course} />
+        ))}
+      </div>
     </div>
   );
 };
